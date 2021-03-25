f282232af2641f7cc683137b476e53a382e64417
WN <allen91.wu@gmail.com>
2021-03-25 22:47:48 +0800

    random picture

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 import discord
 from discord.ext import commands
 import json
+import random
 
 with open('setting.json','r',encoding='utf8') as jfile:
     jdata=json.load(jfile)
@@ -12,6 +13,7 @@ bot=commands.Bot(command_prefix='[')
 async def on_ready():
     print(">> Bot is online <<")
 # print online in terminal
+
 @bot.event
 async def on_member_join(member):
     print(f'{member}join!')
@@ -37,21 +39,32 @@ async def on_member_remove(member):
 @bot.command()
 async def ping(ctx):
    await ctx.send(f'{round(bot.latency*1000)}(ms)')
+   print('sned a message')
 # test send ping float
+
+@bot.command()
+async def 圖片(ctx):
+    random_pic=random.choice(jdata['pic'])
+    pic=discord.File(random_pic)
+    await ctx.send(file=pic)
+# random send picture
 
 @bot.command()
 async def 要不要吃烤肉(ctt):
    await ctt.send('好啊我想吃')
+   print('sned a message')
 # test chat
 
 @bot.command()
 async def 走啊(ctt):
    await ctt.send('哪次不去了')
+   print('sned a message')
 # test chat
 
 @bot.command()
 async def 餓(ctt):
    await ctt.send('吃飯')
+   print('sned a message')
 # test chat
 
 
