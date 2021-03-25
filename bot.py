1eb2147d6481dff705bcde76ab4b7ccdfb8289a3
WN <allen91.wu@gmail.com>
2021-03-25 18:36:04 +0800

    json

--- bot.py
+++ bot.py
@@ -1,6 +1,11 @@
 # Python 3.9.2 64-bit
 import discord
 from discord.ext import commands
+import json
+
+with open('setting.json','r',encoding='utf8') as jfile:
+    jdata=json.load(jfile)
+
 bot=commands.Bot(command_prefix='[')
 
 @bot.event
@@ -14,7 +19,7 @@ async def on_member_join(member):
 
 bot.event
 async def on_member_join(member):
-    channel=bot.get_channel(824585823059574784)
+    channel=bot.get_channel(int(jdata['general_channel']))
     await channel.send(f'{member}join!')
 # send message notice member join in discord
 
@@ -25,7 +30,7 @@ async def on_member_remove(member):
 
 @bot.event
 async def on_member_remove(member):
-    channel=bot.get_channel(824585823059574784)
+    channel=bot.get_channel(jdata['general_channel'])
     await channel.send(f'{member}leave!')
 # send message notice member leave in discord
 
@@ -44,4 +49,4 @@ async def 走啊(ctt):
    await ctt.send('哪次不去了')
 # test chat
 
-bot.run('ODI0NTY0MDQzMTU4MjU3Njk0.YFxNGw.3ApQrtk6_MSCnmQ5IPPR4Iwn-Og')
+bot.run(jdata['TOKEN'])
