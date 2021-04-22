2e1fcba702cc75d91578d09d1f37572427a5428e
allen91wu <allen91.wu@gmail.com>
2021-04-22 20:40:16 +0800

    feat(msg): clean message and say again message with bot

--- cmds/react.py
+++ cmds/react.py
@@ -10,41 +10,13 @@ with open('setting.json','r',encoding='utf8') as jfile:
 class React(Cog_Extension):
 
     @commands.command()
-    async def 要不要吃烤肉(self,ctt):
-        await ctt.send('好啊我想吃')
-        print('sned a message')
-    # chat
-    
-    @commands.command()
-    async def 走啊(self,ctt):
-        await ctt.send('哪次不去了')
-        print('sned a message')
-    # chat
+    async def sayd(self,ctx,*,msg):
+        await ctx.message.delete()
+        await ctx.send(msg)
 
     @commands.command()
-    async def 餓(self,ctt):
-        await ctt.send('吃飯')
-        print('sned a message')
-    # chat
-
-    @commands.command()
-    async def 早安(self,ctt):
-       await ctt.send('早安阿')
-       print('sned a message')
-    # chat
-
-    @commands.command()
-    async def 閉嘴機器人(self,ctt):
-        await ctt.send('你他媽才閉嘴')
-        print('sned a message')
-    # chat
-
-    @commands.command()
-    async def 要一起想像嗎(self,ctt):
-        await ctt.send('你他媽才葉宜')
-        print('sned a message')
-    # chat
-
+    async def clean(self,ctx,num: int):
+        await ctx.channel.purge(limit= num+1)
 
 def setup(bot):
     bot.add_cog(React(bot))
