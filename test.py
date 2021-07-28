e6621e21767b027150d92d054c56399f3d588909
allen91wu <allen91.wu@gmail.com>
2021-07-28 17:37:09 +0800

    test(test.py): remove redundant code

--- test.py
+++ test.py
@@ -1,51 +1,8 @@
 # Python 3.8
-import discord  # noqa: F401
 from discord.ext import commands
-import json
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-with open("setting.json", "r", encoding="utf8") as jfile:
-    jdata = json.load(jfile)
 
 bot = commands.Bot(command_prefix="[")
-
-
-@bot.event
-async def on_ready():
-    print(">> Bot is online <<")
-
-
-@bot.command()
-async def helpme(ctt):
-    await ctt.send(jdata["explain"])
-    print("sned a message")
-
-
-@bot.command()
-async def load(ctx, extension):
-    bot.load_extension(f"cmds.{extension}")
-    await ctx.send(f"loaded {extension} done.")
-    print("sned a message")
-
-
-@bot.command()
-async def unload(ctx, extension):
-    bot.unload_extension(f"cmds.{extension}")
-    await ctx.send(f"un-loaded {extension} done.")
-    print("sned a message")
-
-
-@bot.command()
-async def reload(ctx, extension):
-    bot.reload_extension(f"cmds.{extension}")
-    await ctx.send(f"re-loaded {extension} done.")
-    print("sned a message")
-
 
 for filename in os.listdir("./cmds"):
     if filename.endswith(".py"):
